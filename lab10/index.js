e7614c3a50932ab6fdddf555d392952a9c0704a8
lzielezinska <zielezinska.lucyna@gmail.com>
2020-05-21 09:14:15 +0200

    lab10

---
+++ lab10/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const random_number = Math.random();
+
+app.get("/", (req, res) => {
+   return res.send(random_number);
+});
+
+app.listen(4000, () => {
+    console.log('Server listenig on port 4000');
+});
